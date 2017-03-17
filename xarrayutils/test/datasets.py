d1a4d00f2ba058360ba00fa476a91b4c1b9539a6
Julius Busecke <jb3210@columbia.edu>
2017-03-17 19:43:16 -0400

    fixed lat lon distance (#1)

--- xarrayutils/test/datasets.py
+++ xarrayutils/test/datasets.py
@@ -4,6 +4,7 @@ import pytest
 import xarray as xr
 import numpy as np
 import dask.array as da
+from .. build_grids import dll_dist
 
 a_2d_nan = np.array([
     [1,1,1,3,3,3,5,5],
@@ -19,15 +20,36 @@ ones_2d = np.ones([6,8],dtype=np.float)
 ones_2d_nan      = ones_2d.copy()
 ones_2d_nan[2,2] = np.nan
 
-# for the grid dataset
-grid_i = np.arange(0,8)
-grid_j = np.arange(0,6)
-grid_XC,grid_YC = np.meshgrid(grid_i+0.5,grid_j+0.5)
-grid_XG,grid_YG = np.meshgrid(grid_i,grid_j)
-grid_dxC = np.ones_like(grid_XG)
+# # for the grid dataset in non ll coordinates
+# grid_i = np.arange(0,8)
+# grid_j = np.arange(0,6)
+# grid_XC,grid_YC = np.meshgrid(grid_i+0.5,grid_j+0.5)
+# grid_XG,grid_YG = np.meshgrid(grid_i,grid_j)
+# grid_dxC = np.ones_like(grid_XG)
+# grid_dyC = grid_dxC
+# grid_dxG = grid_dxC
+# grid_dyG = grid_dxC
+
+# for the grid dataset in non ll coordinates
+spacing = 20
+lon = np.arange(0,360,spacing)
+grid_i = np.arange(0,len(lon))
+lat = np.arange(-90,90,spacing)
+grid_j = np.arange(0,len(lat))
+grid_XC,grid_YC = np.meshgrid(lon+spacing/2,lat+spacing/2)
+grid_XG,grid_YG = np.meshgrid(lon,lat)
+grid_dxC = np.ones_like(grid_XG)*spacing
 grid_dxG = grid_dxC
 grid_dyC = grid_dxC
 grid_dyG = grid_dxC
+
+grid_dxC_ll,grid_dyC_ll = dll_dist(grid_dxC,grid_dyC,
+                                    grid_XC,grid_YC,
+                                    xarray=False)
+
+grid_dxG_ll,grid_dyG_ll = dll_dist(grid_dxG,grid_dyG,
+                                    grid_XG,grid_YG,
+                                    xarray=False)
 
 
 dataarrays= {
@@ -45,6 +67,20 @@ dataarrays= {
         'dxC': (['j','i_g'], grid_dxC),
         'dyG': (['j','i_g'], grid_dyG),
         'dyC': (['j_g','i'], grid_dyC)},
+        ),
+    'datagrid_dimtest_ll': xr.Dataset(
+        coords={'j': (['j',], grid_j),
+        'j_g': (['j_g',], grid_j),
+        'i': (['i',], grid_i),
+        'i_g': (['i_g',], grid_i),
+        'XC': (['j','i'], grid_XC),
+        'YC': (['j','i'], grid_YC),
+        'XG': (['j_g','i_g'], grid_XG),
+        'YG': (['j_g','i_g'], grid_YG),
+        'dxG': (['j_g','i'], grid_dxG_ll),
+        'dxC': (['j','i_g'], grid_dxC_ll),
+        'dyG': (['j','i_g'], grid_dyG_ll),
+        'dyC': (['j_g','i'], grid_dyC_ll)},
         ),
     'datagrid_w_attrs': xr.Dataset(
         coords={
@@ -106,6 +142,10 @@ def dataarray_2d_ones_nan(request):
 
 @pytest.fixture(params=['datagrid_dimtest'])
 def datagrid_dimtest(request):
+    return dataarrays[request.param]
+
+@pytest.fixture(params=['datagrid_dimtest_ll'])
+def datagrid_dimtest_ll(request):
     return dataarrays[request.param]
 
 @pytest.fixture(params=['datagrid_w_attrs'])
