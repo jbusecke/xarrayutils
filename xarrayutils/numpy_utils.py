068bac82acd10fd796638d0b7de164a6b51ba2a5
jbusecke <julius@ldeo.columbia.edu>
2017-04-24 17:24:07 -0400

    flake8 corrections

--- xarrayutils/numpy_utils.py
+++ xarrayutils/numpy_utils.py
@@ -1,10 +1,12 @@
 
 from __future__ import print_function
-from future.utils import iteritems
+# from future.utils import iteritems
 import numpy as np
 import scipy.interpolate as spi
 
-def interp_map_regular_grid(a,x,y,x_i,y_i,method='linear',debug=False,wrap=True):
+
+def interp_map_regular_grid(a, x, y, x_i, y_i,
+                            method='linear', debug=False, wrap=True):
     """Interpolates 2d fields from regular grid to another regular grid.
 
     wrap option: pads outer values/coordinates with other side of the array.
@@ -25,60 +27,62 @@ def interp_map_regular_grid(a,x,y,x_i,y_i,method='linear',debug=False,wrap=True)
         y[0] = y[0]-180
         y[-1] = y[-1]+180
 
-        a = a[:,[-1]+range(a.shape[1])+[0]]
-        a = a[[-1]+range(a.shape[0])+[0],:]
+        a = a[:, [-1]+range(a.shape[1])+[0]]
+        a = a[[-1]+range(a.shape[0])+[0], :]
 
     if debug:
-        print('a shape',a.shape)
-        print('x shape',x.shape)
-        print('y shape',y.shape)
-        print('x values',x[:])
-        print('y values',y[:])
-        print('x_i values',x_i[:])
-        print('y_i values',y_i[:])
+        print('a shape', a.shape)
+        print('x shape', x.shape)
+        print('y shape', y.shape)
+        print('x values', x[:])
+        print('y values', y[:])
+        print('x_i values', x_i[:])
+        print('y_i values', y_i[:])
 
-    xx_i,yy_i = np.meshgrid(x_i,y_i)
-    f = spi.RegularGridInterpolator((x, y),a.T,method=method,bounds_error=False)
-    int_points = np.vstack((xx_i.flatten(),yy_i.flatten())).T
+    xx_i, yy_i = np.meshgrid(x_i, y_i)
+    f = spi.RegularGridInterpolator((x, y), a.T,
+                                    method=method, bounds_error=False)
+    int_points = np.vstack((xx_i.flatten(), yy_i.flatten())).T
     a_new = f(int_points)
 
     return a_new.reshape(xx_i.shape)
 
-def interp_map_irregular_grid(a,x,y,x_i,y_i,method='linear',debug=False):
+
+def interp_map_irregular_grid(a, x, y, x_i, y_i, method='linear', debug=False):
     """Interpolates fields from any grid to another grid
     !!! Careful when using this on regular grids.
     Results are not unique and it takes forever.
     Use interp_map_regular_grid instead!!!
     """
-    xx,yy = np.meshgrid(x,y)
-    xx_i,yy_i = np.meshgrid(x_i,y_i)
+    xx, yy = np.meshgrid(x, y)
+    xx_i, yy_i = np.meshgrid(x_i, y_i)
 
     # pad margins to avoid nans in the interpolation
-    xx = xx[:,[-1]+range(xx.shape[1])+[0]]
-    xx = xx[[-1]+range(xx.shape[0])+[0],:]
+    xx = xx[:, [-1]+range(xx.shape[1])+[0]]
+    xx = xx[[-1]+range(xx.shape[0])+[0], :]
 
-    xx[:,0] = xx[:,0]-360
-    xx[:,-1] = xx[:,-1]+360
+    xx[:, 0] = xx[:, 0]-360
+    xx[:, -1] = xx[:, -1]+360
 
-    yy = yy[:,[-1]+range(yy.shape[1])+[0]]
-    yy = yy[[-1]+range(yy.shape[0])+[0],:]
+    yy = yy[:, [-1]+range(yy.shape[1])+[0]]
+    yy = yy[[-1]+range(yy.shape[0])+[0], :]
 
-    yy[0,:] = yy[0,:]-180
-    yy[-1,:] = yy[-1,:]+180
+    yy[0, :] = yy[0, :] - 180
+    yy[-1, :] = yy[-1, :] + 180
 
-    a = a[:,[-1]+range(a.shape[1])+[0]]
-    a = a[[-1]+range(a.shape[0])+[0],:]
+    a = a[:, [-1]+range(a.shape[1])+[0]]
+    a = a[[-1]+range(a.shape[0])+[0], :]
 
     if debug:
-        print('a shape',a.shape)
-        print('x shape',xx.shape)
-        print('y shape',yy.shape)
-        print('x values',xx[0,:])
-        print('y values',yy[:,0])
+        print('a shape', a.shape)
+        print('x shape', xx.shape)
+        print('y shape', yy.shape)
+        print('x values', xx[0, :])
+        print('y values', yy[:, 0])
 
-    points = np.vstack((xx.flatten(),yy.flatten())).T
+    points = np.vstack((xx.flatten(), yy.flatten())).T
     values = a.flatten()
-    int_points = np.vstack((xx_i.flatten(),yy_i.flatten())).T
-    a_new = spi.griddata(points,values,int_points,method=method)
+    int_points = np.vstack((xx_i.flatten(), yy_i.flatten())).T
+    a_new = spi.griddata(points, values, int_points, method=method)
 
     return a_new.reshape(xx_i.shape)
--- xarrayutils/utils.py
+++ xarrayutils/utils.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from future.utils import iteritems
+# from future.utils import iteritems
 import numpy as np
 import xarray as xr
 from scipy.signal import filtfilt, butter, gaussian
@@ -13,20 +13,24 @@ Collection of several useful routines for xarray
 """
 Lower Level implementation in numpy and dask
 """
-def aggregate_w_nanmean(da,weights,blocks,**kwargs):
+
+
+def aggregate_w_nanmean(da, weights, blocks, **kwargs):
     """
     weighted nanmean for xarrays
     """
     # make sure that the missing values are exactly equal to each otherwise
     weights = weights.where(~np.isnan(da))
-    if not np.all(np.isnan(da)==np.isnan(weights)):
-        raise RuntimeError('weights cannot have more missing values then the data array')
+    if not np.all(np.isnan(da) == np.isnan(weights)):
+        raise RuntimeError('weights cannot have more missing values \
+        then the data array')
 
-    weights_sum = aggregate(weights,blocks,func=np.nansum,**kwargs)
-    da_sum   = aggregate(da*weights,blocks,func=np.nansum,**kwargs)
+    weights_sum = aggregate(weights, blocks, func=np.nansum, **kwargs)
+    da_sum = aggregate(da*weights, blocks, func=np.nansum, **kwargs)
     return da_sum/weights_sum
 
-def aggregate(da,blocks,func=np.nanmean,debug=False):
+
+def aggregate(da, blocks, func=np.nanmean, debug=False):
     """
     Performs efficient block averaging in one or multiple dimensions.
     Only works on regular grid dimensions.
@@ -57,7 +61,8 @@ def aggregate(da,blocks,func=np.nanmean,debug=False):
     >>> y = np.arange(-10,10)
     >>> xx,yy = np.meshgrid(x,y)
     >>> z = xx**2-yy**2
-    >>> a = xr.DataArray(da.from_array(z, chunks=(20, 20)),coords={'x':x,'y':y},dims=['y','x'])
+    >>> a = xr.DataArray(da.from_array(z, chunks=(20, 20)),
+                         coords={'x':x,'y':y}, dims=['y','x'])
     >>> print a
 
     <xarray.DataArray 'array-7e422c91624f207a5f7ebac426c01769' (y: 20, x: 20)>
@@ -79,15 +84,18 @@ def aggregate(da,blocks,func=np.nanmean,debug=False):
         Coarsened with: <function mean at 0x111754230>
         Coarsenblocks: [('x', 2), ('y', 10)]
     """
-    # Check if the input is a dask array (I might want to convert this automaticlaly in the future)
-    if not isinstance(da.data,Array):
+    # Check if the input is a dask array (I might want to convert this
+    # automaticlaly in the future)
+    if not isinstance(da.data, Array):
         raise RuntimeError('data array data must be a dask array')
-    #Check data type of blocks
+    # Check data type of blocks
     # TODO write test
     if (not all(isinstance(n[0], str) for n in blocks) or
-        not all(isinstance(n[1], int) for n in blocks)):
-        print('blocks input',str(blocks))
-        raise RuntimeError("block dimension must be dtype(str), e.g. ('lon',4)")
+            not all(isinstance(n[1], int) for n in blocks)):
+
+        print('blocks input', str(blocks))
+        raise RuntimeError("block dimension must be dtype(str), \
+        e.g. ('lon',4)")
 
     # Check if the given array has the dimension specified in blocks
     try:
@@ -96,11 +104,11 @@ def aggregate(da,blocks,func=np.nanmean,debug=False):
         raise RuntimeError("'blocks' contains non matching dimension")
 
     # Check the size of the excess in each aggregated axis
-    blocks = [(a[0],a[1],da.shape[da.get_axis_num(a[0])] % a[1]) for a in blocks]
+    blocks = [(a[0], a[1], da.shape[da.get_axis_num(a[0])] % a[1])
+              for a in blocks]
 
     # for now default to trimming the excess
-    da_coarse = coarsen(func,da.data,block_dict,trim_excess=True)
-
+    da_coarse = coarsen(func, da.data, block_dict, trim_excess=True)
 
     # for now default to only the dims
     new_coords = dict([])
@@ -110,14 +118,17 @@ def aggregate(da,blocks,func=np.nanmean,debug=False):
         new_coords[cc] = da.coords[cc]
         for dd in blocks:
             if dd[0] in list(da.coords[cc].dims):
-                new_coords[cc] = new_coords[cc].isel(**{dd[0]:slice(0,-(1+dd[2]),dd[1])})
+                new_coords[cc] = \
+                    new_coords[cc].isel(**{dd[0]: slice(0, -(1+dd[2]), dd[1])})
 
-    attrs = {'Coarsened with':str(func),'Coarsenblocks':str(blocks)}
-    da_coarse = xr.DataArray(da_coarse,dims=da.dims,coords=new_coords,\
-                    name=da.name,attrs=attrs)
+    attrs = {'Coarsened with': str(func), 'Coarsenblocks': str(blocks)}
+    da_coarse = xr.DataArray(da_coarse, dims=da.dims, coords=new_coords,
+                             name=da.name, attrs=attrs)
     return da_coarse
 
-def fancymean(raw,dim=None,axis=None,method='arithmetic',weights=None,debug=False):
+
+def fancymean(raw, dim=None, axis=None, method='arithmetic',
+              weights=None, debug=False):
     """ extenden mean function for xarray
 
     Applies various methods to estimate mean values
@@ -125,7 +136,7 @@ def fancymean(raw,dim=None,axis=None,method='arithmetic',weights=None,debug=Fals
     dimension with optional weigthing values, which
     can be a coordinate in the passed xarray structure
     """
-    if not isinstance(raw,xr.Dataset) and not isinstance(raw,xr.DataArray):
+    if not isinstance(raw, xr.Dataset) and not isinstance(raw, xr.DataArray):
         raise RuntimeError('input needs to be xarray structure')
 
     # map dim to axis so this works on ndarrays and DataArray/Dataset
@@ -142,28 +153,28 @@ def fancymean(raw,dim=None,axis=None,method='arithmetic',weights=None,debug=Fals
     if dim is not None:
         if axis is not None:
             raise ValueError('cannot set both `dim` and `axis`')
-        if isinstance(raw,xr.Dataset):
+        if isinstance(raw, xr.Dataset):
             axis = raw[raw.data_vars.keys()[0]].get_axis_num(dim)
             if debug:
-                print('dim ',dim,' changed to axis ',axis)
-        elif isinstance(raw,xr.DataArray):
+                print('dim ', dim, ' changed to axis ', axis)
+        elif isinstance(raw, xr.DataArray):
             axis = raw.get_axis_num(dim)
             if debug:
-                print('dim ',dim,' changed to axis ',axis)
+                print('dim ', dim, ' changed to axis ', axis)
 
     if debug:
-        print('axis',axis)
+        print('axis', axis)
 
-
-    if weights==None:
+    if weights == None:
         w = 1
     elif isinstance(weights, basestring):
         w = raw[weights]
-    elif isinstance(weights,np.ndarray):
-        w = xr.DataArray(np.ones_like(raw.data),coords=raw.coords,dims=raw.dims)
+    elif isinstance(weights, np.ndarray):
+        w = xr.DataArray(np.ones_like(raw.data),
+                         coords=raw.coords, dims=raw.dims)
 
     # make sure the w array is the same size as the raw array
-    # This way also nans will be propagated correctly in a bidirectional fashion
+    # This way also nans will be propagated correctly in a bidirectional way
     ones = raw.copy()
     ones = (ones*0)+1
     w = w*ones
@@ -176,50 +187,57 @@ def fancymean(raw,dim=None,axis=None,method='arithmetic',weights=None,debug=Fals
         down = w
         out = up.sum(axis=axis)/down.sum(axis=axis)
     elif method == 'geometric':
-        w = w.where(raw>0)
-        raw = raw.where(raw>0)
+        w = w.where(raw > 0)
+        raw = raw.where(raw > 0)
         up = np.log10(raw)*w
         down = w
         out = 10**(up.sum(axis=axis)/down.sum(axis=axis))
     elif method == 'harmonic':
-        w = w.where(raw!=0)
-        raw = raw.where(raw!=0)
+        w = w.where(raw != 0)
+        raw = raw.where(raw != 0)
         up = w/raw
         down = w
         out = down.sum(axis=axis)/up.sum(axis=axis)
     if debug:
-        print('w',w.shape)
-        print('raw',raw.shape)
-        print('up',up.shape)
-        print('down',down.shape)
-        print('out',out.shape)
+        print('w', w.shape)
+        print('raw', raw.shape)
+        print('up', up.shape)
+        print('down', down.shape)
+        print('out', out.shape)
 
     return out
 
-def timefilter(xr_in,steps,step_spec,timename='time',filtertype='gaussian',stdev=0.1):
+
+def timefilter(xr_in, steps,
+               step_spec, timename='time',
+               filtertype='gaussian', stdev=0.1):
     timedim = xr_in.dims.index(timename)
     dt = np.diff(xr_in.time.data[0:2])[0]
     cut_dt = np.timedelta64(steps, step_spec)
 
-    if filtertype=='gaussian':
+    if filtertype == 'gaussian':
         win_length = (cut_dt/dt).astype(int)
         a = [1.0]
-        win = gaussian(win_length,std=(float(win_length)*stdev))
+        win = gaussian(win_length, std=(float(win_length)*stdev))
         b = win/win.sum()
-        if np.nansum(win)==0:
+        if np.nansum(win) == 0:
             raise RuntimeError('window to short for time interval')
-            print('win_length',str(win_length))
-            print('stddev',str(stdev))
-            print('win',str(win))
+            print('win_length', str(win_length))
+            print('stddev', str(stdev))
+            print('win', str(win))
 
-    filtered = filtfilt(b,a, xr_in.data,axis=timedim,padtype=None,padlen=0)
-    out = xr.DataArray(filtered,dims=xr_in.dims,coords=xr_in.coords,attrs=xr_in.attrs)
-    out.attrs.update({'filterlength': (steps,step_spec),'filtertype': filtertype})
+    filtered = filtfilt(b, a, xr_in.data, axis=timedim, padtype=None, padlen=0)
+    out = xr.DataArray(filtered, dims=xr_in.dims, coords=xr_in.coords,
+                       attrs=xr_in.attrs)
+    out.attrs.update({'filterlength': (steps, step_spec),
+                      'filtertype': filtertype})
     if xr_in.name:
         out.name = xr_in.name+'_lowpassed'
     return out
 
-def extractBoxes(da,bo,xname=None,yname =None,xdim='i',ydim='j',tname ='time',method='arithmetic',weights=None):
+
+def extractBoxes(da, bo, xname=None, yname=None, xdim='i', ydim='j',
+                 tname='time', method='arithmetic', weights=None):
     """ Extracts average timeseries from boxes
 
 
@@ -236,30 +254,37 @@ def extractBoxes(da,bo,xname=None,yname =None,xdim='i',ydim='j',tname ='time',me
     xname and yname have to be specified if coordinates are of differnt shape
 
     tname -- coordinate name for time dimention (default: 'time')
-    method -- choice of method to compute spatial mean. See docs of TracerProcessing.py/xarray_fancymean
+    method -- choice of method to compute spatial mean. See docs of
+                TracerProcessing.py/xarray_fancymean
     weights --
     """
 
-    if not type(xname)==type(yname):
+    if not type(xname) == type(yname):
             raise RuntimeError('xname and yname need to be the same type')
 
     timeseries = []
-    for ii,bb in enumerate(bo.keys()):
+    for ii, bb in enumerate(bo.keys()):
         box = bo[bb]
-        if xname==None:
-            box_dict ={xdim:slice(box[0],box[1]),ydim:slice(box[2],box[3])}
-            temp = fancymean(da.loc[box_dict],dim=(xdim,ydim),method=method,weights=weights)
+        if xname is None:
+            box_dict = {xdim: slice(box[0], box[1]),
+                        ydim: slice(box[2], box[3])}
+            temp = fancymean(da.loc[box_dict], dim=(xdim, ydim),
+                             method=method, weights=weights)
         else:
-            mask = np.logical_and(np.logical_and(da[xname]>box[0],da[xname]<box[1]),\
-                              np.logical_and(da[yname]>box[2],da[yname]<box[3]))
-            temp = fancymean(da.where(mask),dim=(xdim,ydim),method=method,weights=weights)
+            mask = np.logical_and(np.logical_and(da[xname] > box[0],
+                                                 da[xname] < box[1]),
+                                  np.logical_and(da[yname] > box[2],
+                                                 da[yname] < box[3]))
+            temp = fancymean(da.where(mask), dim=(xdim, ydim),
+                             method=method, weights=weights)
 
         timeseries.append(temp)
-    out = xr.concat(timeseries,'boxname')
+    out = xr.concat(timeseries, 'boxname')
     out['boxname'] = bo.keys()
     return out
 
-def composite(data,index,bounds):
+
+def composite(data, index, bounds):
     """
     Composites Dataarray according to index
 
@@ -284,19 +309,22 @@ def composite(data,index,bounds):
     --------
     TODO
     """
-    if isinstance(bounds,int):
+    if isinstance(bounds, int):
         bounds = float(bounds)
 
-    if isinstance(bounds,float):
-        bounds = [-bounds*np.std(index),bounds*np.std(index)]
+    if isinstance(bounds, float):
+        bounds = [-bounds*np.std(index), bounds*np.std(index)]
 
-    if len(bounds)!=2:
+    if len(bounds) != 2:
         raise RuntimeError('bounds can only have 1 or two elements')
 
     comp_name = 'composite'
-    zones = [index>=bounds[1],np.logical_and(index<bounds[1],index>=bounds[0]),index<bounds[0]]
-    zones_coords = ['high','neutral','low']
-    out = xr.concat([data.where(z) for z in zones],comp_name)
+    zones = [index >= bounds[1],
+             np.logical_and(index < bounds[1],
+                            index >= bounds[0]),
+             index < bounds[0]]
+    zones_coords = ['high', 'neutral', 'low']
+    out = xr.concat([data.where(z) for z in zones], comp_name)
     out[comp_name] = zones_coords
 
     out.attrs['IndexName'] = index.name
@@ -304,48 +332,51 @@ def composite(data,index,bounds):
 
     return out
 
-def corrmap(a,b,shifts=0,\
-                   a_x_dim='i',a_y_dim='j',\
-                   a_x_coord=None,a_y_coord=None,\
-                   b_x_dim='i',b_y_dim='j',\
-                   b_x_coord=None,b_y_coord=None,\
-                   t_dim='time',debug=True):
+
+def corrmap(a, b, shifts=0,
+            a_x_dim='i', a_y_dim='j',
+            a_x_coord=None, a_y_coord=None,
+            b_x_dim='i', b_y_dim='j',
+            b_x_coord=None, b_y_coord=None,
+            t_dim='time', debug=True):
 
     """
     a -- input
     b -- target ()
 
     TODO
-    This thing is slow. I can most likely rewrite this with numpy.apply_along_axis
+    This thing is slow. I can most likely rewrite this with \
+    numpy.apply_along_axis
 
 
     """
 
     from scipy.stats import linregress
 
-    if not type(a_x_coord)==type(a_y_coord):
+    if not type(a_x_coord) == type(a_y_coord):
         raise RuntimeError('a_x_coord and a_y_coord need to be the same type')
 
-    if not type(b_x_coord)==type(b_y_coord):
+    if not type(b_x_coord) == type(b_y_coord):
         raise RuntimeError('a_x_coord and a_y_coord need to be the same type')
 
-    if isinstance(shifts,int):
+    if isinstance(shifts, int):
         shifts = [shifts]
 
     # determine if the timseries is a timeseries or a 3d array
-    if len(b.shape)==3:
-        arrayswitch=True
-    elif len(b.shape)==1:
-        arrayswitch=False
+    if len(b.shape) == 3:
+        arrayswitch = True
+    elif len(b.shape) == 1:
+        arrayswitch = False
     else:
-        raise RuntimeWarning('this only works with a timseries or map of timeseries')
+        raise RuntimeWarning('this only works with a timseries \
+            or map of timeseries')
 
     # shift timeseries
     slope = []
     corr = []
     p_value = []
 
-    for sh,shift in enumerate(shifts):
+    for sh, shift in enumerate(shifts):
         shifted_b = b.shift(time=shift)
 
         s = a.mean(dim=t_dim).copy()
@@ -363,10 +394,12 @@ def corrmap(a,b,shifts=0,\
         for ii in range(len(a[a_x_dim])):
             for jj in range(len(a[a_y_dim])):
 
-                # Define the 'input' (position in a) correctly, accounting for the possibility that the
-                # lat/lon position can be defined in the coordinates or dimensions
+                # Define the 'input' (position in a) correctly, accounting for
+                # the possibility that the
+                # lat/lon position can be defined in the coordinates
+                # or dimensions
                 # interp timeseries onto the data.time
-                in_a = a[{a_x_dim:ii,a_y_dim:jj}]
+                in_a = a[{a_x_dim: ii, a_y_dim: jj}]
 
                 if arrayswitch:
                     if not a_x_coord:
@@ -378,37 +411,48 @@ def corrmap(a,b,shifts=0,\
 
                     # rename the dimensions so it can be reindexed
                     if not b_x_coord:
-                        in_b = xr.DataArray(shifted_b.data,\
-                                            coords={'xdim':shifted_b[b_x_dim].data,\
-                                                    'ydim':shifted_b[b_y_dim].data,'time':shifted_b.time.data},\
-                                           dims = ['time','ydim','xdim'])
+                        in_b = xr.DataArray(shifted_b.data,
+                                            coords={'xdim':
+                                                    shifted_b[b_x_dim].data,
+                                                    'ydim':
+                                                    shifted_b[b_y_dim].data,
+                                                    'time':
+                                                    shifted_b.time.data},
+                                            dims=['time', 'ydim', 'xdim'])
                     else:
                         raise RuntimeError('Not implemented yet')
-                        # This would have to be acomplished by a mask of some sort
+                        # This would have to be acomplished by a mask of some
+                        # sort
                         # (with some tolerance around the input position)
 
-                    #extract the matching timeseries
-                    in_b = in_b.sel(xdim=in_x,ydim=in_y,method='nearest')
-                    reindexed_b = in_b.reindex_like(in_a.time,method='nearest')
+                    # extract the matching timeseries
+                    in_b = in_b.sel(xdim=in_x, ydim=in_y, method='nearest')
+                    reindexed_b = in_b.reindex_like(in_a.time,
+                                                    method='nearest')
                 else:
-                    reindexed_b = shifted_b.reindex_like(in_a.time,method='nearest')
+                    reindexed_b = shifted_b.reindex_like(in_a.time,
+                                                         method='nearest')
 
                 x = reindexed_b.data
                 y = in_a.data
 
-                idx = np.logical_and(~np.isnan(y),~np.isnan(x))
+                idx = np.logical_and(~np.isnan(y), ~np.isnan(x))
                 if y[idx].size:
-                    s[{a_x_dim:ii,a_y_dim:jj}],_,c[{a_x_dim:ii,a_y_dim:jj}],\
-                        p[{a_x_dim:ii,a_y_dim:jj}],_ = linregress(x[idx],y[idx])
+                    s[{a_x_dim: ii, a_y_dim: jj}], _, c[{a_x_dim: ii,
+                                                        a_y_dim: jj}],\
+                        p[{a_x_dim: ii, a_y_dim: jj}], _ = linregress(x[idx],
+                                                                      y[idx])
         slope.append(s)
         corr.append(c)
         p_value.append(p)
 
-    out_s = xr.concat(slope,'timeshifts')
-    out_s['timeshifts'] = shifts # !!! I think this is a bug...this should be possible with
-    out_c = xr.concat(corr,'timeshifts')
+    out_s = xr.concat(slope, 'timeshifts')
+    out_s['timeshifts'] = shifts
+    # !!! I think this is a bug...this should be
+    # possible with
+    out_c = xr.concat(corr, 'timeshifts')
     out_c['timeshifts'] = shifts
-    out_p = xr.concat(p_value,'timeshifts',)
+    out_p = xr.concat(p_value, 'timeshifts',)
     out_p['timeshifts'] = shifts
 
-    return out_c,out_p,out_s
+    return out_c, out_p, out_s
