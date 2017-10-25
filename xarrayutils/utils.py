0da6d7b1bdb85d31034d4c8eb4401e1a9b6858c4
jbusecke <julius@ldeo.columbia.edu>
2017-10-25 18:00:29 -0400

    bugfix3

--- xarrayutils/utils.py
+++ xarrayutils/utils.py
@@ -472,9 +472,9 @@ def extract_surf(da_ind, da_target, surf_val, dim,
     if constant_dims is not None:
         for ll in constant_dims:
             constant_nan_dims[ll] = 0
-        nanmask = da_ind[constant_nan_dims].isnan()
+        nanmask = xr.ufunc.isnan(da_ind[constant_nan_dims])
     else:
-        nanmask = da_ind.isnan()
+        nanmask = xr.ufunc.isnan(da_ind)
 
     ind = find_surf_ind(da_ind.fillna(fill_value), surf_val, dim)
     # Expand ind into full dimensions
