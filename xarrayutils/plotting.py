edbc136bd737424e982db271d28a393af1d831f8
jbusecke <julius@ldeo.columbia.edu>
2018-05-14 20:27:28 -0400

    added tsdiagram to plotting

--- xarrayutils/plotting.py
+++ xarrayutils/plotting.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import gsw
 
 
 def depth_logscale(ax, yscale=400, ticks=None):
@@ -104,3 +105,95 @@ def box_plot(box, ax=None, split_detection='True', **kwargs):
     elif not x_split and not y_split:
         ax.plot([box[0], box[0], box[1], box[1], box[0]],
                  [box[2], box[3], box[3], box[2], box[2]], **kwargs)
+
+
+def draw_dens_contours_teos10(sigma='sigma0', add_labels=True, ax=None,
+                              density_grid=20, dens_interval=1.0,
+                              salt_on_x=True, slim=None, tlim=None,
+                              contour_kwargs={}, c_label_kwargs={}, **kwargs):
+    """draws density contours on the current plot.
+    Assumes that the salinity and temperature values are given as SA and CT.
+    Needs documentation... """
+    if ax is None:
+        ax = plt.gca()
+
+    if sigma not in ['sigma%i' % s for s in range(5)]:
+        raise ValueError('Sigma function has to be one of `sigma0`...`sigma4` \
+                         is: %s' % (sigma))
+
+    # get salt (default: xaxis) and temp (default: yaxis) limits
+    if salt_on_x:
+        if not slim:
+            slim = ax.get_xlim()
+        if not tlim:
+            tlim = ax.get_ylim()
+        x = np.linspace(*slim, density_grid)
+        y = np.linspace(*tlim, density_grid)
+    else:
+        if not tlim:
+            tlim = ax.get_xlim()
+        if not slim:
+            slim = ax.get_ylim()
+        x = np.linspace(*slim, density_grid)
+        y = np.linspace(*tlim, density_grid)
+
+    if salt_on_x:
+        ss, tt = np.meshgrid(x, y)
+    else:
+        tt, ss = np.meshgrid(x, y)
+
+    sigma_func = getattr(gsw, sigma)
+
+    sig = sigma_func(ss, tt)
+
+    levels = np.arange(np.floor(sig.min()), np.ceil(sig.max()), dens_interval)
+
+    c_kwarg_defaults = dict(levels=levels, colors='0.4',
+                            linestyles='--', linewidths=0.5)
+    c_kwarg_defaults.update(kwargs)
+    c_kwarg_defaults.update(contour_kwargs)
+
+    c_label_kwarg_defaults = dict(fmt='%.02f')
+    c_label_kwarg_defaults.update(kwargs)
+    c_label_kwarg_defaults.update(c_label_kwargs)
+
+    ch = ax.contour(x, y, sig, **c_kwarg_defaults)
+    ax.clabel(ch, **c_label_kwarg_defaults)
+
+    ct_label = 'Conservative Temperature [$^{\circ}C$]'
+    sa_label = 'Absolute Salinity [$g/kg$]'
+    if add_labels:
+        if salt_on_x:
+            ax.set_xlabel(sa_label)
+            ax.set_ylabel(ct_label)
+        else:
+            ax.set_ylabel(sa_label)
+            ax.set_xlabel(ct_label)
+        plt.text(0.05, 0.05, '$\sigma_{%s}$' % (sigma[-1]), fontsize=14,
+                 verticalalignment='center',
+                 horizontalalignment='center', transform=ax.transAxes,
+                 color=c_kwarg_defaults['colors'])
+
+
+def tsdiagram(salt, temp, color=None, size=None,
+              lon=None, lat=None, pressure=None,
+              convert_teos10=True, ts_kwargs={},
+              ax=None, **kwargs):
+    if ax is None:
+        ax = plt.gca()
+
+    if convert_teos10:
+        if any([a is None for a in [lon, lat, pressure]]):
+            raise ValueError('when converting to teos10 variables, \
+                             input for lon, lat and pressure is needed')
+        else:
+            salt = gsw.SA_from_SP(salt, pressure, lon, lat)
+            temp = gsw.CT_from_pt(salt, temp)
+
+    scatter_kw_defaults = dict(s=size, c=color)
+    scatter_kw_defaults.update(kwargs)
+    s = ax.scatter(salt, temp, **scatter_kw_defaults)
+    draw_dens_contours_teos10(ax=ax, **ts_kwargs)
+    if color:
+        if len(color) > 1:
+            plt.colorbar(s)
