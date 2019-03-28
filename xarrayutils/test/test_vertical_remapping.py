7c9abb7b4df10b2869e9cf2f87c30e26a106eb1f
jbusecke <julius@ldeo.columbia.edu>
2019-03-28 17:54:33 -0400

    Added new conservative remapping function

---
+++ xarrayutils/test/test_vertical_remapping.py
@@ -0,0 +1,69 @@
+import numpy as np
+import xarray as xr
+from xarrayutils.vertical_remapping import _groupby_vert, xr_1d_groupby
+
+
+import pytest
+
+bins = [
+    np.arange(26, 28, 0.1),  # regular spaced bins
+    np.array([26.5, 26.6, 26.7, 26.8, 26.9, 27, 27.5, 28.0]),
+]
+
+depth = np.arange(10)
+dens = np.linspace(26, 29, len(depth))
+
+
+@pytest.mark.parametrize("bins", bins)
+def test_groupby_vert(bins):
+    # Create dummy DataArray
+    data = np.random.rand(len(depth))
+    da = xr.DataArray(data, coords=[("depth", depth)])
+    da_group = xr.DataArray(dens, coords=[("depth", depth)])
+    da_group.name = "density"  # needed for groupby_bins
+
+    truth = da.groupby_bins(da_group, bins).sum()
+    new = _groupby_vert(da.data, da_group.data, bins, np.nansum)
+
+    np.testing.assert_allclose(new, truth.data)
+
+    # test with different functions...
+
+    #
+    # # for the bigger tests
+    # Needs to fail for unnamed da.
+
+
+@pytest.mark.parametrize("bins", bins)
+def test_xr_1d_groupby(bins):
+    data = np.random.rand(2, 2, len(depth), 3)
+    data_dens = abs(data) + dens[np.newaxis, np.newaxis, :, np.newaxis]
+    da = xr.DataArray(
+        data,
+        coords=[
+            ("x", range(2)),
+            ("y", range(2)),
+            ("depth", depth),
+            ("time", range(3)),
+        ],
+    )
+    da_group = xr.DataArray(
+        data_dens,
+        coords=[
+            ("x", range(2)),
+            ("y", range(2)),
+            ("depth", depth),
+            ("time", range(3)),
+        ],
+    )
+    da_group_unnamed = da_group.copy()
+    da_group.name = "density"
+    # test also without
+    sample = dict(x=1, y=0, time=1)  # randomize
+
+    check = da.isel(**sample).groupby_bins(da_group.isel(**sample), bins).sum()
+    test = xr_1d_groupby(da, da_group, bins, "depth", np.nansum).isel(**sample)
+
+    np.testing.assert_allclose(check.data, test.data)
+    with pytest.raises(ValueError):
+        xr_1d_groupby(da, da_group_unnamed, bins, "depth", np.nansum)
---
+++ xarrayutils/vertical_remapping.py
@@ -0,0 +1,170 @@
+import numpy as np
+import xarray as xr
+
+#########################
+# Density layer version #
+#########################
+# for later: handle the intervals just like groupby. For now workaround
+# Does not handle decreasing values, this is probably related to the above
+
+
+def _groupby_vert(data, group_data, bins, func):
+    # Replicates the behaviour of xarrays `groupby_bins` along one dimension
+    # with numpy
+    axis = -1  # xr.apply_ufunc transposes core dims to the end
+    layers = []
+
+    for b in range(len(bins) - 1):
+        bb = [bins[b], bins[b + 1]]
+        # This should be customizable like in groupby
+        mask = np.logical_and(bb[0] < group_data, bb[1] >= group_data)
+        data_masked = data.copy()
+        data_masked[~mask] = np.nan
+        nanmask = np.all(~mask, axis=axis)
+        layer = func(data_masked, axis=axis)
+        # there might be an exeption when this is run on a 1d vector...
+        # but for now this works...
+        # I formerly did this with ma.masked_array,
+        # but somehow that did not work with apply_ufunc
+
+        # special treatment for 1d input arrays
+        if not isinstance(layer, np.ndarray):
+            layer = np.array(layer)
+
+        layer[nanmask] = np.nan
+        layers.append(layer)
+    return np.stack(layers, axis=-1)
+
+
+def xr_1d_groupby(data, group_data, bins, dim, func=np.nansum):
+    """Short summary.
+
+    Parameters
+    ----------
+    data : type
+        Description of parameter `data`.
+    group_data : type
+        Description of parameter `group_data`.
+    bins : type
+        Description of parameter `bins`.
+    dim : type
+        Description of parameter `dim`.
+    func : type
+        Description of parameter `func` (the default is np.nansum).
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+    bin_name = group_data.name
+    bin_dim = "%s_layers" % bin_name
+    bin_center = (bins[:-1] + bins[1:]) / 2
+
+    name = group_data.name
+    if name is None:
+        raise ValueError("`group_data` array must have name")
+
+    remapped = xr.apply_ufunc(
+        _groupby_vert,
+        data,
+        group_data,
+        bins,
+        func,
+        input_core_dims=[[dim], [dim], ["bins"], []],
+        output_core_dims=[[bin_dim]],
+        dask="parallelized",
+        output_dtypes=[data.dtype],
+        output_sizes={bin_dim: len(bins) - 1},
+    )
+
+    remapped.coords[bin_dim] = bin_center
+    remapped.coords[bin_dim + "_lower"] = (bin_dim, bins[:-1])
+    remapped.coords[bin_dim + "_upper"] = (bin_dim, bins[1:])
+    #     remapped.coords[bin_dim+'_intervals'] = (['']list(zip(,bins[1:]))
+    return remapped
+
+
+###############
+# Needs tests #
+###############
+
+
+def xr_remapping(
+    da_data,
+    da_group,
+    bins,
+    dim,
+    distance_coord,
+    content_var=False,
+    return_average=True,
+):
+    """Performs remapping into another tracer coordinate system.
+
+    Parameters
+    ----------
+    da_data : xr.DataArray
+        Data array to be remapped.
+    da_group : xr.DataArray
+        Data array of values to remap onto (e.g. potential density).
+    bins : array-like
+        Spacing for new coordinates, in units of `da_group`. `da_data` values
+        are binned between values of `bins`.
+    dim : str
+        Dimension along remapping is performed (e.g. depth).
+    distance_coord : str
+        Name of coordinate in `da_data` which contains distances along `dim`.
+        Required to acurately weight data points.
+    content_var : bool
+        Option for preweighted values. If True, `distance_coord` will not be
+        multiplied with `da_data` before binning  (the default is False).
+    return_average : bool
+        Option to return layer averages (True) or layer integrals (False).
+
+    Returns
+    -------
+    type
+        Description of returned object.
+
+    """
+    da_data = da_data.copy()
+    da_group = da_group.copy()
+    if not (set(da_data.dims) == set(da_group.dims)):
+        raise ValueError(
+            "`da_data` and `da_group` do not have identical dims. Please interpolate broadcast appropriately before remapping"
+        )
+
+    da_thick = da_data.coords[distance_coord].copy()
+    da_dim = da_data.coords[dim].copy()
+
+    # make sure that the thickness data is not counted anywhere else but where there is data
+    thick_name = da_thick.name  # seems to be overwritten by the line below
+    da_thick = da_thick * ((da_data * 0) + 1)
+    # Same for the layer position
+    da_dim = da_dim * ((da_data * 0) + 1)
+
+    # Weight da_dim and da_data (only for content_var=False) with da_thick
+    da_dim = da_dim * da_thick
+    if not content_var:
+        da_data = da_data * da_thick
+
+    func = np.nansum
+
+    data_remapped = xr_1d_groupby(da_data, da_group, bins, dim, func=func)
+    thickness = xr_1d_groupby(da_thick, da_group, bins, dim, func=func)
+    layer_pos = xr_1d_groupby(da_dim, da_group, bins, dim, func=func)
+
+    if return_average:
+        data_remapped = data_remapped / thickness
+
+    data_remapped.coords[
+        "%s_layer_%s" % (da_group.name, thick_name)
+    ] = thickness
+    # calculate the mean depth of the layer
+    data_remapped.coords["%s_layer_%s" % (da_group.name, dim)] = (
+        layer_pos / thickness
+    )
+    data_remapped.name = da_data.name
+
+    return data_remapped
