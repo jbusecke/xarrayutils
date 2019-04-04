9997a3437d2ea2645e1607b4f9aa21e01e188907
jbusecke <julius@ldeo.columbia.edu>
2019-04-04 16:09:53 -0400

    Added options for axis arrow

--- xarrayutils/plotting.py
+++ xarrayutils/plotting.py
@@ -7,7 +7,7 @@ import string
 import cartopy
 
 
-def xr_violinplot(ds, ax=None, x_dim='xt_ocean', width=1, color='0.5'):
+def xr_violinplot(ds, ax=None, x_dim="xt_ocean", width=1, color="0.5"):
     """Wrapper of matplotlib violinplot for xarray.DataArray.
 
     Parameters
@@ -40,18 +40,24 @@ def xr_violinplot(ds, ax=None, x_dim='xt_ocean', width=1, color='0.5'):
 
     if ax is None:
         ax = plt.gca()
-    vp = ax.violinplot(y, x, widths=width, showextrema=False,
-                       showmedians=False, showmeans=True)
-    [item.set_facecolor(color) for item in vp['bodies']]
+    vp = ax.violinplot(
+        y,
+        x,
+        widths=width,
+        showextrema=False,
+        showmedians=False,
+        showmeans=True,
+    )
+    [item.set_facecolor(color) for item in vp["bodies"]]
 
-    for item in ['cmaxes', 'cmins', 'cbars', 'cmedians', 'cmeans']:
+    for item in ["cmaxes", "cmins", "cbars", "cmedians", "cmeans"]:
         if item in vp.keys():
             vp[item].set_edgecolor(color)
 
     return vp
 
 
-def axis_arrow(ax, x_loc, text):
+def axis_arrow(ax, x_loc, text, arrowprops, **kwargs):
     """Puts an arrow pointing at `x_loc` onto (but outside of ) the xaxis of
     a plot.For now only works on xaxis and on the top. Modify when necessary
 
@@ -63,14 +69,28 @@ def axis_arrow(ax, x_loc, text):
         Position of the arrow (in units of `ax` x-axis).
     text : str
         Text next to arrow.
-
+    arrowprops: dict
+        Additional arguments to pass to arrowprops.
+        See mpl.axes.annotate for details.
+    kwargs:
+        additional keyword arguments passed to ax.annotate
 
     """
+    ar_props = dict(dict(fc="k", lw=1.5, ec=None))
+    ar_props.update(arrowprops)
+
     tform = blended_transform_factory(ax.transData, ax.transAxes)
-    ax.annotate(text, xy=[x_loc, 1], xytext=(x_loc, 1.25),
-                xycoords=tform, textcoords=tform,
-                ha='center', va='center',
-                arrowprops=dict(fc='k', lw=1.5, ec=None))
+    ax.annotate(
+        text,
+        xy=[x_loc, 1],
+        xytext=(x_loc, 1.25),
+        xycoords=tform,
+        textcoords=tform,
+        ha="center",
+        va="center",
+        arrowprops=ar_props,
+        **kwargs
+    )
 
 
 def letter_subplots(axes, start_idx=0, box_color=None, **kwargs):
@@ -91,15 +111,22 @@ def letter_subplots(axes, start_idx=0, box_color=None, **kwargs):
     """
 
     for ax, letter in zip(axes.flat, list(string.ascii_lowercase)[start_idx:]):
-        t = ax.text(0.1, 0.85, letter+')', horizontalalignment='center',
-                    verticalalignment='center', transform=ax.transAxes,
-                    **kwargs)
+        t = ax.text(
+            0.1,
+            0.85,
+            letter + ")",
+            horizontalalignment="center",
+            verticalalignment="center",
+            transform=ax.transAxes,
+            **kwargs
+        )
         if box_color:
             t.set_bbox(dict(facecolor=box_color, alpha=0.5, edgecolor=None))
 
 
-def map_util_plot(ax, land_color='0.7', coast_color='0.3',
-                  lake_alpha=0.5, labels=False):
+def map_util_plot(
+    ax, land_color="0.7", coast_color="0.3", lake_alpha=0.5, labels=False
+):
     """Helper tool to add good default map to cartopy axes.
 
     Parameters
@@ -124,34 +151,39 @@ def map_util_plot(ax, land_color='0.7', coast_color='0.3',
     # add option for gridlines and labelling
 
 
-def center_lim(ax, which='y'):
-    if which == 'y':
+def center_lim(ax, which="y"):
+    if which == "y":
         lim = np.array(ax.get_ylim())
         ax.set_ylim(np.array([-1, 1]) * abs(lim).max())
-    elif which == 'x':
+    elif which == "x":
         lim = np.array(ax.get_xlim())
         ax.set_xlim(np.array([-1, 1]) * abs(lim).max())
-    elif which in ['xy', 'yx']:
-        center_lim(ax, 'x')
-        center_lim(ax, 'y')
+    elif which in ["xy", "yx"]:
+        center_lim(ax, "x")
+        center_lim(ax, "y")
     else:
-        raise ValueError('`which` is not in (`x,`y`, `xy`) found %s' % which)
+        raise ValueError("`which` is not in (`x,`y`, `xy`) found %s" % which)
 
 
 def depth_logscale(ax, yscale=400, ticks=None):
     if ticks is None:
         ticks = [0, 100, 250, 500, 1000, 2500, 5000]
-    ax.set_yscale('symlog', linthreshy=yscale)
+    ax.set_yscale("symlog", linthreshy=yscale)
     ticklabels = [str(a) for a in ticks]
     ax.set_yticks(ticks)
     ax.set_yticklabels(ticklabels)
     ax.invert_yaxis()
 
 
-def plot_line_shaded_std(x, y, std_y, horizontal=True,
-                         ax=None,
-                         line_kwargs=dict(),
-                         fill_kwargs=dict()):
+def plot_line_shaded_std(
+    x,
+    y,
+    std_y,
+    horizontal=True,
+    ax=None,
+    line_kwargs=dict(),
+    fill_kwargs=dict(),
+):
     """Plot wrapper to draw line for y and shaded patch according to std_y.
     The shading represents one std on each side of the line...
 
@@ -194,20 +226,19 @@ def plot_line_shaded_std(x, y, std_y, horizontal=True,
     else:
         p = ax.plot(y, x, **line_defaults)
 
-    fill_defaults = {'color': p[-1].get_color(),
-                     'alpha': 0.35}
+    fill_defaults = {"color": p[-1].get_color(), "alpha": 0.35}
 
     # Apply defaults but respect input
     fill_defaults.update(fill_kwargs)
 
     if horizontal:
-        ax.fill_between(x, y-std_y, y+std_y, **fill_defaults)
+        ax.fill_between(x, y - std_y, y + std_y, **fill_defaults)
     else:
-        ax.fill_betweenx(x, y-std_y, y+std_y, **fill_defaults)
+        ax.fill_betweenx(x, y - std_y, y + std_y, **fill_defaults)
     return p
 
 
-def box_plot(box, ax=None, split_detection='True', **kwargs):
+def box_plot(box, ax=None, split_detection="True", **kwargs):
     """plots box despite coordinate discontinuities.
     INPUT
     -----
@@ -221,8 +252,10 @@ def box_plot(box, ax=None, split_detection='True', **kwargs):
     """
 
     if len(box) != 4:
-        raise RuntimeError("'box' must be a 4 element np.array, \
-            describing the box corners [x1, x2, y1, y2]")
+        raise RuntimeError(
+            "'box' must be a 4 element np.array, \
+            describing the box corners [x1, x2, y1, y2]"
+        )
     xlim = plt.gca().get_xlim()
     ylim = plt.gca().get_ylim()
     x_split = False
@@ -239,65 +272,89 @@ def box_plot(box, ax=None, split_detection='True', **kwargs):
             y_split = True
 
     if y_split and not x_split:
-        ax.plot([box[0], box[0], box[1], box[1], box[0]],
-                 [ylim[1], box[2], box[2], ylim[1], ylim[1]], **kwargs)
+        ax.plot(
+            [box[0], box[0], box[1], box[1], box[0]],
+            [ylim[1], box[2], box[2], ylim[1], ylim[1]],
+            **kwargs
+        )
 
-        ax.plot([box[0], box[0], box[1], box[1], box[0]],
-                 [ylim[0], box[3], box[3], ylim[0], ylim[0]], **kwargs)
+        ax.plot(
+            [box[0], box[0], box[1], box[1], box[0]],
+            [ylim[0], box[3], box[3], ylim[0], ylim[0]],
+            **kwargs
+        )
 
     elif x_split and not y_split:
-        ax.plot([xlim[1], box[0], box[0], xlim[1], xlim[1]],
-                 [box[2], box[2], box[3], box[3], box[2]], **kwargs)
+        ax.plot(
+            [xlim[1], box[0], box[0], xlim[1], xlim[1]],
+            [box[2], box[2], box[3], box[3], box[2]],
+            **kwargs
+        )
 
-        ax.plot([xlim[0], box[1], box[1], xlim[0], xlim[0]],
-                 [box[2], box[2], box[3], box[3], box[2]], **kwargs)
+        ax.plot(
+            [xlim[0], box[1], box[1], xlim[0], xlim[0]],
+            [box[2], box[2], box[3], box[3], box[2]],
+            **kwargs
+        )
 
     elif x_split and y_split:
-        ax.plot([xlim[1], box[0], box[0]], [box[2], box[2], ylim[1]],
-                 **kwargs)
+        ax.plot([xlim[1], box[0], box[0]], [box[2], box[2], ylim[1]], **kwargs)
 
-        ax.plot([xlim[0], box[1], box[1]], [box[2], box[2], ylim[1]],
-                 **kwargs)
+        ax.plot([xlim[0], box[1], box[1]], [box[2], box[2], ylim[1]], **kwargs)
 
-        ax.plot([xlim[1], box[0], box[0]], [box[3], box[3], ylim[0]],
-                 **kwargs)
+        ax.plot([xlim[1], box[0], box[0]], [box[3], box[3], ylim[0]], **kwargs)
 
-        ax.plot([xlim[0], box[1], box[1]], [box[3], box[3], ylim[0]],
-                 **kwargs)
+        ax.plot([xlim[0], box[1], box[1]], [box[3], box[3], ylim[0]], **kwargs)
 
     elif not x_split and not y_split:
-        ax.plot([box[0], box[0], box[1], box[1], box[0]],
-                 [box[2], box[3], box[3], box[2], box[2]], **kwargs)
+        ax.plot(
+            [box[0], box[0], box[1], box[1], box[0]],
+            [box[2], box[3], box[3], box[2], box[2]],
+            **kwargs
+        )
 
-def dict2box(di, xdim='lon', ydim='lat'):
-    return np.array([di[xdim].start, di[xdim].stop,
-                     di[ydim].start, di[ydim].stop])
+
+def dict2box(di, xdim="lon", ydim="lat"):
+    return np.array(
+        [di[xdim].start, di[xdim].stop, di[ydim].start, di[ydim].stop]
+    )
 
 
-def box_plot_dict(di, xdim='lon', ydim='lat', **kwargs):
+def box_plot_dict(di, xdim="lon", ydim="lat", **kwargs):
     """plot box from xarray selection dict e.g.
     `{'xdim':slice(a, b), 'ydim':slice(c,d), ...}`"""
 
     # extract box from dict
-    box  = dict2box(di, xdim=xdim, ydim=ydim)
+    box = dict2box(di, xdim=xdim, ydim=ydim)
     # plot
     box_plot(box, **kwargs)
 
 
-
-def draw_dens_contours_teos10(sigma='sigma0', add_labels=True, ax=None,
-                              density_grid=20, dens_interval=1.0,
-                              salt_on_x=True, slim=None, tlim=None,
-                              contour_kwargs={}, c_label_kwargs={}, **kwargs):
+def draw_dens_contours_teos10(
+    sigma="sigma0",
+    add_labels=True,
+    ax=None,
+    density_grid=20,
+    dens_interval=1.0,
+    salt_on_x=True,
+    slim=None,
+    tlim=None,
+    contour_kwargs={},
+    c_label_kwargs={},
+    **kwargs
+):
     """draws density contours on the current plot.
     Assumes that the salinity and temperature values are given as SA and CT.
     Needs documentation... """
     if ax is None:
         ax = plt.gca()
 
-    if sigma not in ['sigma%i' % s for s in range(5)]:
-        raise ValueError('Sigma function has to be one of `sigma0`...`sigma4` \
-                         is: %s' % (sigma))
+    if sigma not in ["sigma%i" % s for s in range(5)]:
+        raise ValueError(
+            "Sigma function has to be one of `sigma0`...`sigma4` \
+                         is: %s"
+            % (sigma)
+        )
 
     # get salt (default: xaxis) and temp (default: yaxis) limits
     if salt_on_x:
@@ -326,12 +383,13 @@ def draw_dens_contours_teos10(sigma='sigma0', add_labels=True, ax=None,
 
     levels = np.arange(np.floor(sig.min()), np.ceil(sig.max()), dens_interval)
 
-    c_kwarg_defaults = dict(levels=levels, colors='0.4',
-                            linestyles='--', linewidths=0.5)
+    c_kwarg_defaults = dict(
+        levels=levels, colors="0.4", linestyles="--", linewidths=0.5
+    )
     c_kwarg_defaults.update(kwargs)
     c_kwarg_defaults.update(contour_kwargs)
 
-    c_label_kwarg_defaults = dict(fmt='%.02f')
+    c_label_kwarg_defaults = dict(fmt="%.02f")
     c_label_kwarg_defaults.update(kwargs)
     c_label_kwarg_defaults.update(c_label_kwargs)
 
@@ -339,18 +397,35 @@ def draw_dens_contours_teos10(sigma='sigma0', add_labels=True, ax=None,
     ax.clabel(ch, **c_label_kwarg_defaults)
 
     if add_labels:
-        plt.text(0.05, 0.05, '$\sigma_{%s}$' % (sigma[-1]), fontsize=14,
-                 verticalalignment='center',
-                 horizontalalignment='center', transform=ax.transAxes,
-                 color=c_kwarg_defaults['colors'])
+        plt.text(
+            0.05,
+            0.05,
+            "$\sigma_{%s}$" % (sigma[-1]),
+            fontsize=14,
+            verticalalignment="center",
+            horizontalalignment="center",
+            transform=ax.transAxes,
+            color=c_kwarg_defaults["colors"],
+        )
 
 
-def tsdiagram(salt, temp, color=None, size=None,
-              lon=None, lat=None, pressure=None,
-              convert_teos10=True, ts_kwargs={},
-              ax=None, fig=None, draw_density_contours=True,
-              draw_cbar=True, add_labels=True,
-              **kwargs):
+def tsdiagram(
+    salt,
+    temp,
+    color=None,
+    size=None,
+    lon=None,
+    lat=None,
+    pressure=None,
+    convert_teos10=True,
+    ts_kwargs={},
+    ax=None,
+    fig=None,
+    draw_density_contours=True,
+    draw_cbar=True,
+    add_labels=True,
+    **kwargs
+):
     if ax is None:
         ax = plt.gca()
 
@@ -358,17 +433,19 @@ def tsdiagram(salt, temp, color=None, size=None,
         fig = plt.gcf()
 
     if convert_teos10:
-        temp_label = 'Conservative Temperature [$^{\circ}C$]'
-        salt_label = 'Absolute Salinity [$g/kg$]'
+        temp_label = "Conservative Temperature [$^{\circ}C$]"
+        salt_label = "Absolute Salinity [$g/kg$]"
         if any([a is None for a in [lon, lat, pressure]]):
-            raise ValueError('when converting to teos10 variables, \
-                             input for lon, lat and pressure is needed')
+            raise ValueError(
+                "when converting to teos10 variables, \
+                             input for lon, lat and pressure is needed"
+            )
         else:
             salt = gsw.SA_from_SP(salt, pressure, lon, lat)
             temp = gsw.CT_from_pt(salt, temp)
     else:
-        temp_label = 'Potential Temperature [$^{\circ}C$]'
-        salt_label = 'Practical Salinity [$g/kg$]'
+        temp_label = "Potential Temperature [$^{\circ}C$]"
+        salt_label = "Practical Salinity [$g/kg$]"
 
     if add_labels:
         ax.set_xlabel(salt_label)
@@ -382,9 +459,12 @@ def tsdiagram(salt, temp, color=None, size=None,
     if draw_cbar and color is not None:
         if isinstance(color, str) or isinstance(color, tuple):
             pass
-        elif isinstance(color, list) or isinstance(color, np.ndarray) or \
-                isinstance(color, xr.DataArray):
+        elif (
+            isinstance(color, list)
+            or isinstance(color, np.ndarray)
+            or isinstance(color, xr.DataArray)
+        ):
             fig.colorbar(s, ax=ax)
         else:
-            raise RuntimeError('`color` not recognized. %s' % type(color))
+            raise RuntimeError("`color` not recognized. %s" % type(color))
     return s
