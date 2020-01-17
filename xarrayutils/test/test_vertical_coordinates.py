d9f9377abef2fc73c22e12fe6c72f53464be815f
jbusecke <julius@ldeo.columbia.edu>
2020-01-17 11:35:01 -0500

    Finished tests

--- xarrayutils/test/test_vertical_coordinates.py
+++ xarrayutils/test/test_vertical_coordinates.py
@@ -4,6 +4,7 @@ import pytest
 from xarrayutils.vertical_coordinates import (
     conservative_remap,
     linear_interpolation_remap,
+    linear_interpolation_regrid,
     _strip_dim,
     _coord_interp,
     _regular_interp,
@@ -218,7 +219,6 @@ def test_linear_interpolation_remap(
         z_regridded_dim=z_regridded_dim,
         output_dim=output_dim,
     )
-    print(remapped)
     # select random sample for 3 times
     for iteration in range(3):
         sample = {
@@ -325,3 +325,144 @@ def test_coord_interp():
     data = np.array([30, np.nan, np.nan])
     z_new = _coord_interp(z, data, 30)
     assert np.isnan(z_new)
+
+
+@pytest.mark.parametrize("multi_dim", [True, False])
+@pytest.mark.parametrize("coords", [True, False])
+@pytest.mark.parametrize("z_dim", ["z", "blob"])
+@pytest.mark.parametrize("target_value_dim", ["temp", "blab"])
+@pytest.mark.parametrize("output_dim", ["remapped", "boink"])
+@pytest.mark.parametrize("z_bounds", [False, True])
+@pytest.mark.parametrize("z_bounds_dim", ["z_bounds", "bawoosh"])
+@pytest.mark.parametrize(
+    "dat, src, tar",
+    [
+        (
+            # first example, going from low res to high res
+            # NOTE: the target bounds always need to cover all values of the
+            # source, otherwise properties are not conserved
+            np.array([30, 12.3, 5]),
+            np.array([4.5, 9, 23, 40]),
+            np.array([0, 2, 4, 10, 11, 13.4, 23, 55.6, 80, 100]),
+        ),
+        (
+            # second example, going from high res to low res
+            # NOTE: the target bounds always need to cover all values of the
+            # source, otherwise properties are not conserved
+            np.array([30, 12.3, 5, 2, -1, 4]),
+            np.array([4.5, 9, 23, 45.6, 46, 70, 90]),
+            np.array([0, 10, 30, 100]),
+        ),
+        (
+            # third example, using negative depth values sorted
+            np.array([30, 12.3, 5, 2, -1, 4]),
+            np.array([-90, -70, -46, -45.6, -23, -9, -2]),
+            np.array([-100, -10, -4, -0]),
+        ),
+        (
+            # forth example, using negative depth values unsorted
+            # this is not supported atm, because I need to find a way to sort a
+            # multidim depth array consistently (would be easy with 1d.)
+            np.array([30, 12.3, 5, 2, -1, 4]),
+            np.array([-4.5, -9, -23, -45.6, -46, -70, -80]),
+            np.array([-0, -10, -40, -100]),
+        ),
+        (
+            # fifth example, using negative AND positive depth values sorted
+            np.array([30, 12.3, 5, 2, -1, 4]),
+            np.array([-90, -70, -45.6, -23, 0, 1, 2]),
+            np.array([-100, -10, 0, 10]),
+        ),
+    ],
+)
+def test_linear_interpolation_regrid(
+    dat,
+    src,
+    tar,
+    coords,
+    multi_dim,
+    z_dim,
+    target_value_dim,
+    output_dim,
+    z_bounds,
+    z_bounds_dim,
+):
+    # this is a lot of setup but the test is simple. Create some random input arrays
+    # of different shapes, and see if for random samples, the profile data is what we
+    # would expect from the utility function. All other issues should be addressed within
+    # that function.
+
+    # create test datasets
+    src = np.array(src)
+    # reconstruct the source depth center from cell bounds
+    z_raw = 0.5 * (src[1:] + src[0:-1])
+    if coords:
+        data = xr.DataArray(dat, coords=[(z_dim, z_raw)])
+        z_source_bnds = xr.DataArray(src, coords=[(z_bounds_dim, src)])
+        z_target = xr.DataArray(tar, coords=[(target_value_dim, tar)])
+        z_source = xr.DataArray(z_raw, coords=[(z_dim, z_raw)])
+    else:
+        data = xr.DataArray(np.array(dat), dims=[z_dim])
+        z_source_bnds = xr.DataArray(src, dims=[z_bounds_dim])
+        z_target = xr.DataArray(tar, dims=[target_value_dim])
+        z_source = xr.DataArray(z_raw, dims=[z_dim])
+
+    # the target and data should always have other dimensions
+    data = random_broadcast(data)
+    z_target = random_broadcast(z_target)
+    if multi_dim:
+        z_source_bnds = random_broadcast(z_source_bnds)
+        z_source = random_broadcast(z_source)
+
+    if z_bounds:
+        bnds = z_source_bnds
+    else:
+        bnds = None
+
+    regridded = linear_interpolation_regrid(
+        z_source,
+        data,
+        z_target,
+        z_dim=z_dim,
+        target_value_dim=target_value_dim,
+        output_dim=output_dim,
+        z_bounds=bnds,
+        z_bounds_dim=z_bounds_dim,
+    )
+    # select random sample for 2 times
+    for iteration in range(2):
+        sample = {
+            "test_a": np.random.randint(0, len(regridded.test_a)),
+            "test_b": np.random.randint(0, len(regridded.test_b)),
+            "test_c": np.random.randint(0, len(regridded.test_c)),
+        }
+        profile = regridded.isel(**sample).load().data
+        if multi_dim:
+            z = z_source.isel(**sample)
+        else:
+            z = z_source
+
+        target = z_target.isel(**sample)
+        d = data.isel(**sample)
+
+        if z_bounds:
+            pad_left = bnds[{z_bounds_dim: 0}]
+            pad_right = bnds[{z_bounds_dim: -1}]
+            if multi_dim:
+                pad_left = pad_left.isel(**sample)
+                pad_right = pad_right.isel(**sample)
+            pad_left = pad_left.data
+            pad_right = pad_right.data
+        else:
+            pad_left = pad_right = None
+
+        expected_profile = _coord_interp(
+            z.data, d.data, target.data, pad_left=pad_left, pad_right=pad_right,
+        )
+
+        np.testing.assert_allclose(profile, expected_profile)
+
+    # check output coords
+    np.testing.assert_allclose(
+        regridded.coords[output_dim].data, z_target.coords[target_value_dim].data
+    )
